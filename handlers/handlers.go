4200564f65a1d565955c4b8978f8453f589f6849
Francisco Zambrano <franciscozambrano@outlook.es>
2020-07-25 02:13:02 -0500

    Register user handlers correct

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
+	"github.com/zsfrancisco/twittor-backend/middlew"
+	"github.com/zsfrancisco/twittor-backend/routers"
 	"log"
 	"net/http"
 	"os"
@@ -11,6 +13,9 @@ import (
 /* Handlers the routers of the application */
 func Handlers() {
 	router := mux.NewRouter()
+
+	router.HandleFunc("/register", middlew.CheckBD(routers.Register)).Methods("POST")
+
 	PORT := os.Getenv("PORT")
 	if PORT == "" {
 		PORT = "8080"
